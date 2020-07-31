2b6dcaac0596489e4f00ee2785c44bce03c951ed
Diego M. Rodrigues <diegomr86@gmail.com>
2020-07-31 02:41:39 -0300

    Melhora página de login

---
+++ layouts/login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="login-layout">
+    <Nuxt />
+  </div>
+</template>
+<style lang="sass">
+  .login-layout
+    background-color: #1E2C18
+    color: #fff
+    min-height: 100vh
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="container text-center mt-4">
-    <Logo />
+  <div class="container text-center pt-4">
+    <b-img v-if="site && site.logo" :src="site.logo.url" />
+    <Logo v-else />
     <h1 class="title mt-4">
-      Terrakrya CMS
+      {{ site ? site.name : 'Terrakrya CMS'}}
     </h1>
     <span class="subtitle">
       Entre com seus dados para acessar
@@ -15,7 +16,7 @@
         <b-form-input v-model="form.password" type="password" />
       </b-form-group>
       <b-form-group>
-        <b-button type="submit">
+        <b-button type="submit" variant="primary">
           Entrar
         </b-button>
       </b-form-group>
@@ -26,6 +27,7 @@
 <script>
 import mixinGlobal from '@/mixins/global'
 export default {
+  layout: 'login',
   mixins: [mixinGlobal],
   data () {
     return {
@@ -33,6 +35,11 @@ export default {
         email: '',
         password: ''
       }
+    }
+  },
+  computed: {
+    site () {
+      return this.$store.state.site
     }
   },
   methods: {
