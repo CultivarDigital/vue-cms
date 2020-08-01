4426c8b0e9b513361de143c8b96cc668d6ee1ce8
Diego M. Rodrigues <diegomr86@gmail.com>
2020-08-01 19:12:21 -0300

    teste

--- components/site/Posts.vue
+++ components/site/Posts.vue
@@ -3,9 +3,9 @@
     <h3 class="text-center mb-4 mt-4">NOTÍCIAS</h3>
     <b-row>
       <b-col v-for="(post, index) in posts" :key="post._id" md="4">
-        <!-- <transition name="slide-fade">
+        <transition name="slide-fade">
           <nuxt-link :to="'/noticias/' + post.slug">
-            <b-card v-if="index >= (page * 3) && index < ((page * 3) + 3)" no-body img-top>
+            <!-- <b-card v-if="index >= (page * 3) && index < ((page * 3) + 3)" no-body img-top>
               <div class="img">
                 <b-card-img :src="post.picture ? post.picture.url : null" :alt="post.name" />
               </div>
@@ -17,9 +17,9 @@
                   {{ post.description }}
                 </b-card-text>
               </b-card-body>
-            </b-card>
+            </b-card> -->
           </nuxt-link>
-        </transition> -->
+        </transition>
       </b-col>
     </b-row>
     <arrows-left @click="prev()" />
