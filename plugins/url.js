bdab19f366850f6ce310f7eb3a5b1c9ed97a39f1
Diego M. Rodrigues <diegomr86@gmail.com>
2020-07-31 21:35:38 -0300

    prod test

--- plugins/url.js
+++ plugins/url.js
@@ -1,9 +1,6 @@
 export default ({ $axios, req }) => {
-  // console.log('ASDFASDFASDFASDFASDF ASDF ASDF ASDF ASD FASDF')
-  // console.log(process.env.NODE_ENV)
-  // if (process.env.NODE_ENV === 'production') {
-  //   console.log('http://' + req.headers.host)
-  //   $axios.setBaseURL('http://' + req.headers.host)
-  // }
-  // console.log($axios.defaults.baseURL)
+  if (process.env.NODE_ENV === 'production') {
+    $axios.setBaseURL('http://' + req.headers.host)
+  }
+  console.log($axios.defaults.baseURL)
 }
