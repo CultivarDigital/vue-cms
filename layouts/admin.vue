e4b4619156b2ca7680e9d64958926420bc8352ac
dloopes <dl@loopes.com.br>
2021-01-29 22:55:28 -0300

    fix itens menu order

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -8,12 +8,10 @@
           <b-collapse id="header-admin" is-nav>
             <b-navbar-nav>
               <b-nav-item v-if="$auth.hasScope('super')" to="/admin/sites">Sites</b-nav-item>
-              <b-nav-item v-if="$auth.hasScope('admin')" to="/admin/categories">Categorias</b-nav-item>
-              <b-nav-item v-if="$auth.hasScope('admin')" to="/admin/menus">Menus</b-nav-item>
               <b-nav-item v-if="$auth.hasScope('admin') && $store.state.site" :to="'/admin/sites/' + $store.state.site._id + '/edit'">Site</b-nav-item>
               <b-nav-item v-if="$auth.hasScope('admin')" to="/admin/menus">Menus</b-nav-item>
-              <b-nav-item v-if="$auth.hasScope('admin')" to="/admin/tags">Tags</b-nav-item>
               <b-nav-item v-if="$auth.hasScope('admin')" to="/admin/categories">Categorias</b-nav-item>
+              <b-nav-item v-if="$auth.hasScope('admin')" to="/admin/tags">Tags</b-nav-item>
               <b-nav-item v-if="$auth.hasScope('admin')" to="/admin/pages">Páginas</b-nav-item>
               <b-nav-item v-if="$auth.hasScope('admin')" to="/admin/medias">Biblioteca</b-nav-item>
               <b-nav-item to="/admin/users">Usuários</b-nav-item>
