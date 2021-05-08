5724969159be771428776b9ac5c5038a119f2bea
dloopes <dl@loopes.com.br>
2021-05-07 21:30:52 -0300

    retira bcard de componente banner

--- components/site/Banners.vue
+++ components/site/Banners.vue
@@ -2,13 +2,8 @@
   <div v-if="items.length" class="banners-component">
     <b-carousel :interval="7000" :indicators="items.length > 1" class="banners">
       <template v-for="(item, index) in items">
-        <b-carousel-slide v-if="item" :key="index">
-          <b-card :img-src="item.url" :img-alt="item.title" img-left>
-            <b-card-text>
-              <h4 class="title">{{ item.title }}</h4>
-              <p v-if="item.description" class="text-secondary">{{ (item.description) | truncate(250) }}</p>
-            </b-card-text>
-          </b-card>
+        <b-carousel-slide v-if="item" :key="index" :img-src="item.url">
+          <h2 v-if="item.title">{{ item.title }}</h2>
         </b-carousel-slide>
       </template>
     </b-carousel>
