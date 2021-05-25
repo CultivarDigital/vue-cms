1278ec13cfc656452f961a279ee83bf6a5aa8eed
Diego M. Rodrigues <diegomr86@gmail.com>
2021-05-24 23:19:08 -0300

    Personaliza aplicação

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -5,9 +5,9 @@
       <!-- <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/categories">Categorias</b-list-group-item> -->
       <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/pages">Páginas</b-list-group-item>
       <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/menus">Menus</b-list-group-item>
-      <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/posts">Notícias</b-list-group-item>
-      <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/medias">Biblioteca</b-list-group-item>
-      <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/events">Agenda</b-list-group-item>
+      <!-- <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/posts">Notícias</b-list-group-item> -->
+      <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/medias">Midiateca</b-list-group-item>
+      <!-- <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/events">Agenda</b-list-group-item> -->
       <b-list-group-item v-if="$auth.user.role === 'admin'" to="/admin/ecommerce">Loja</b-list-group-item>
       <b-list-group-item to="/admin/contacts">Contatos</b-list-group-item>
       <b-list-group-item to="/admin/users">Usuários</b-list-group-item>
