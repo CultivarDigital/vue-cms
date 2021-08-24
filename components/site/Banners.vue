7d12873b758b63db931e019371e68d5025cbbc25
Diego M. Rodrigues <diegomr86@gmail.com>
2021-08-23 23:00:34 -0300

    Adiciona container nos textos do banner

--- components/site/Banners.vue
+++ components/site/Banners.vue
@@ -3,12 +3,14 @@
     <b-carousel :interval="7000" :indicators="items.length > 1" class="banners">
       <template v-for="(item, index) in items">
         <b-carousel-slide v-if="item" :key="index" :img-src="item.url" :img-alt="item.title">
-          <h3 v-if="item.title" class="title">{{ item.title }}</h3>
-          <p v-if="item.description" class="mb-0">{{ (item.description) | truncate(250) }}</p>
-          <template v-if="item.link">
-            <b-btn v-if="item.link.startsWith('http')" :href="item.link" target="_blank" variant="secondary" class="mt-md-4 btn-home">{{ item.link_title || 'Saiba mais' }}</b-btn>
-            <b-btn v-else :to="item.link" variant="secondary" class="mt-md-4 btn-home">{{ item.link_title || 'Saiba mais' }}</b-btn>
-          </template>
+          <div class="container">
+            <h3 v-if="item.title" class="title">{{ item.title }}</h3>
+            <p v-if="item.description" class="mb-0">{{ (item.description) | truncate(250) }}</p>
+            <template v-if="item.link">
+              <b-btn v-if="item.link.startsWith('http')" :href="item.link" target="_blank" variant="secondary" class="mt-md-4 btn-home">{{ item.link_title || 'Saiba mais' }}</b-btn>
+              <b-btn v-else :to="item.link" variant="secondary" class="mt-md-4 btn-home">{{ item.link_title || 'Saiba mais' }}</b-btn>
+            </template>
+          </div>
         </b-carousel-slide>
       </template>
     </b-carousel>
