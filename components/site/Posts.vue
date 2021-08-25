b2fce698c7b58850f54cdf8a122d19a8a7cfb7f3
Diego M. Rodrigues <diegomr86@gmail.com>
2021-08-25 20:16:26 -0300

    Corrige cards de notícias

--- components/site/Posts.vue
+++ components/site/Posts.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="posts-component pt-4">
     <b-row>
-      <b-col v-for="post in posts" :key="post._id" lg="6" class="mb-2">
+      <b-col v-for="post in posts" :key="post._id" lg="4" md="6" class="mb-4">
         <b-card :img-src="post.picture ? post.picture.thumb : null" :img-alt="post.title">
           <b-card-text>
-            <h5>{{ post.title }}</h5>
-            <p class="text-primary"><b-link :to="'/noticias/' + post.slug" class="stretched-link text-dark">{{ (post.description || post.content) | truncate(60) }}</b-link></p>
-            <tags :tags="post.tags" @click="filterbyTag" />
+            <h5 class="mb-0">
+              <b-link :to="'/noticias/' + post.slug" class="stretched-link">{{ post.title }}</b-link>
+            </h5>
+            <p v-if="post.description" class="mt-2 mb-0">{{ post.description | truncate(60) }}</p>
           </b-card-text>
         </b-card>
       </b-col>
