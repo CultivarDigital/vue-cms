c23290d3e5366c9877afe43fd878ff25f9bb2e26
Diego M. Rodrigues <diegomr86@gmail.com>
2022-01-10 10:07:31 -0300

    Finaliza algoritimo de importação

--- api/routes/medias.js
+++ api/routes/medias.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router()
+const axios = require('axios')
+const wget = require('node-wget')
 const auth = require('../config/auth')
 const mendeley = require('../../data/mendeley.json')
+const mendeleyFiles = require('../../data/mendeley-files.json')
 const docTypes = require('../../data/document-types.json')
 const Media = mongoose.model('Media')
+const Attachment = mongoose.model('Attachment')
 
 const fieldOptions = (docs, column) => {
   const values = {}
@@ -28,6 +32,8 @@ const fieldArrayOptions = (docs, column) => {
 
 router.get('/import', async (req, res) => {
   const medias = {
+    categories: [],
+    tags: [],
     langs: [],
     languages: [],
     fields: [],
@@ -79,6 +85,33 @@ router.get('/import', async (req, res) => {
         })
     }
 
+    if (m.categories) {
+      m.categories = m.categories.map(category => {
+        if (category === 'economia') {
+          return 'Economia'
+        }
+        if (category === 'Nutriçao e saude' || category === 'Nutriçao e saúde' || category === 'Nutrição e Saúde') {
+          return 'Nutrição e saúde'
+        }
+        if (category === 'Organização Comunitária') {
+          return 'Organização comunitária'
+        }
+        if (category === 'questões ambientais' || category === 'Questões Ambientais') {
+          return 'Questões ambientais'
+        }
+        if (category === 'Sanitario') {
+          return 'Sanitário'
+        }
+        if (category === 'Saúde e Nutrição') {
+          return 'Saúde e nutrição'
+        }
+        if (category === 'tecnologia e inovação' || category === 'Tecnologia e Inovação') {
+          return 'Tecnologia e inovação'
+        }
+
+        return category
+      })
+    }
     m.notes = d.notes
     m.pages = d.pages
     m.patent_legal_status = d.patent_legal_status
@@ -98,11 +131,57 @@ router.get('/import', async (req, res) => {
     Object.keys(d).forEach(k => {
       fields[k] = true
     })
-
+    m.documents = []
+    if (d.websites) {
+      for (const website of d.websites) {
+        const attachment = new Attachment({
+          type: 'documents',
+          url: website,
+          title: website
+        })
+        await attachment.save()
+        m.docs.push(attachment._id)
+      }
+    }
+    const files = mendeleyFiles.filter(file => file.document_id === d.id)
+    for (const file of files) {
+      const resp = await axios.request({
+        url: 'https://www.mendeley.com/reference-manager/api/file/' + file.id + '/',
+        method: 'get',
+        headers: {
+          Cookie: 'msso_check=4:081e83cc-57cf-469a-bc62-5d97118b4982; accessToken=MSwxNjQxNjkyOTc1OTkxLDYzODUxNjY0MSw2NzMyLGFsbCwsLGE2NmNhM2JkODI3N2Q0NDkxMTBiYjQ1NWYwZDkxYjAzZmFhZGd4cnFhLDAwZDQwMzQ3LWIyZjMtM2M0YS05NmY1LTg2MTBlMTc2Yzk5OCx2ZzFJMTd0SGxETjBreklyclRzMW1HZzZVRVU; msso=4:081e83cc-57cf-469a-bc62-5d97118b4982; aa_usr={"accessType":"ae:REG:U_P:GUEST:","accountId":"ae:293744","accountName":"ae:Mendeley Guests","userId":"ae:95453126"}; node_session=eyJpZCI6ImE3YzdjYjU4LTU0NjktNDc2Ni1hNWRiLTkwNDg4MjY4Y2Y0YSIsInN0YXRlIjoiYThiMGU2Yjg3NDQyNjFlMzBhOTYzZGUwNzNkNGEyNWYiLCJyZWZlcnJlclVybCI6Imh0dHBzOi8vd3d3Lm1lbmRlbGV5LmNvbS9yZWZlcmVuY2UtbWFuYWdlci9saWJyYXJ5L2FsbC1yZWZlcmVuY2VzLyIsImlzU29mdFNpZ25JbiI6ZmFsc2UsInByb21wdCI6Im5vbmUiLCJhbmFseXRpY3NJbmZvIjpudWxsLCJpbmR2SWRlbnRpdHkiOiJSRUciLCJpZFRva2VuSWF0IjoiMjVhODNjMTRkMGY0OTg2YzQyYzAzYjc5YmQiLCJpbmNsdWRlUHJvbXB0ZWRTdGF0dXMiOm51bGx9; node_session.sig=RvxG0XYk1ebGN6cQ_jZtCwWjmWw; OptanonAlertBoxClosed=2022-01-09T01:12:36.261Z; AMCVS_4D6368F454EC41940A4C98A6@AdobeOrg=1; at_check=true; AMCV_4D6368F454EC41940A4C98A6@AdobeOrg=-2121179033|MCMID|33596307266216881461098006702080173584|MCAAMLH-1642295909|4|MCAAMB-1642295909|RKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y|MCOPTOUT-1641698309s|NONE|MCAID|NONE|MCCIDH|-778269422|vVersion|5.3.0|MCIDTS|19002; __cf_bm=rhPEI5eQfNKrlgFgrj9.7eQfh8RIiMC4hYpTUt16g7Q-1641691631-0-AVgCQmrnQW3U+4YIhPVPOdlNX+ja8gneMHMW0DcoIppXowp3l2s0Hj/Lc6fDcfpayUv4IY/ymvz6mkjZZfRn5Sc=; mbox=session#b38fe29baa8744098273bd3f9c0570f9#1641694227|PC#b38fe29baa8744098273bd3f9c0570f9.34_0#1704937167; OptanonConsent=isGpcEnabled=0&datestamp=Sat+Jan+08+2022+22:39:35+GMT-0300+(Brasilia+Standard+Time)&version=6.28.0&isIABGlobal=false&hosts=&consentId=f4df9a01-1b02-45ff-9b82-25b1c1853d7f&interactionCount=1&landingPath=NotLandingPage&groups=1:1,3:1,2:1,4:1&geolocation=BR;GO&AwaitingReconsent=false; els-aa-debugmode=true; s_pers= v8=1641692377576|1736300377576; v8_s=First%20Visit|1641694177576; c19=md%3Arefman%3Alibrary|1641694177589; v68=1641692377328|1641694177603;; s_sess= s_cpc=0; s_sq=; s_cc=true; c21=no%20criteria%20set; e13=no%20criteria%20set%3A1; c13=added-desc; e41=1; s_ppvl=md%253Arefman%253Alibrary%2C100%2C100%2C571%2C538%2C571%2C1366%2C768%2C1%2CL; s_ppv=md%253Arefman%253Alibrary%253Areader%253Apanel%253Ainfo%2C100%2C100%2C571%2C538%2C571%2C1366%2C768%2C1%2CL;'
+        }
+      })
+      if (resp && resp.data && resp.data.url) {
+        wget({
+          url: resp.data.url,
+          dest: 'api/uploads/documents/' + file.id + '/' + file.file_name, // destination path or path with filenname, default is ./
+          timeout: 2000000 // duration to wait for request fulfillment in milliseconds, default is 2 seconds
+        },
+        function (error, response, body) {
+          if (error) {
+            console.log('--- error:')
+            console.log(error) // error encountered
+          } else {
+            console.log('Baixado: ' + file.file_name)
+          }
+        }
+        )
+      }
+      const attachment = new Attachment({
+        type: 'documents',
+        url: 'api/uploads/documents/' + file.id + '/' + file.file_name,
+        title: file.file_name
+      })
+      await attachment.save()
+      m.docs.push(attachment._id)
+    }
     await m.save()
     medias.list.push(m)
   }
 
+  medias.categories = fieldArrayOptions(medias.list, 'categories')
+  medias.tags = fieldArrayOptions(mendeley, 'tags')
   medias.langs = fieldOptions(mendeley, 'language')
   medias.languages = fieldArrayOptions(medias.list, 'languages')
 
